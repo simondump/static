934e19046e30cba96fef2addd7718fc54577e998
Simon Reinisch <simon@reinisch.io>
2026-05-28 16:30:50 +0200

    feat: exit early on errors and fix template
    allow manual workflow trigger
    fixate and update pnpm version

--- apps/ghcr-pulls/app.ts
+++ apps/ghcr-pulls/app.ts
@@ -4,6 +4,8 @@ import { dirname, join } from 'node:path';
 
 const log = (str: string) => console.log(`[${new Date().toISOString()}] ghcr-pulls: ${str}`);
 
+const error = (str: string) => console.error(`[${new Date().toISOString()}] ghcr-pulls: ${str}`);
+
 const packages = ['simonwep/ocular/pkgs/container/ocular'];
 const dir = dirname(fileURLToPath(import.meta.url));
 const dist = join(dir, 'dist');
@@ -12,9 +14,20 @@ const template = await readFile(join(dir, 'template.svg'), 'utf8');
 await mkdir(dist, { recursive: true }).catch(() => null);
 
 for (const pkg of packages) {
-  const data = await fetch(`https://github.com/${pkg}`).then((res) => res.text());
+  const data = await fetch(`https://github.com/${pkg}`)
+    .then((res) => res.text())
+    .catch(() => '');
+
   const trimmed = data.trim().replace(/\s/g, '');
   const count = /Totaldownloads.*?h3.*?>(.*?)</.exec(trimmed)?.[1];
+
+  if (!count) {
+    error(`failed fetch or extract downloads for ${pkg}`);
+    continue;
+  } else if (!/^[\d.]+\w$/.test(count)) {
+    error(`failed to parse downloads for ${pkg}`);
+    continue;
+  }
 
   const values = {
     count: `${count} docker pulls`,
@@ -24,5 +37,5 @@ for (const pkg of packages) {
   const badge = template.replace(/\{(.*?)}/g, (_, key) => values[key as keyof typeof values] ?? '');
   const containerName = pkg.split('/').pop();
   await writeFile(join(dist, `${containerName}.svg`), badge);
-  log(`generated ${containerName}.svg`);
+  log(`generated ${containerName}.svg with ${count} downloads`);
 }
